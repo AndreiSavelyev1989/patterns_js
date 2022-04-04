a2ba7114c500a8765e15194efcf58afc42ff7667
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2022-04-04 18:03:16 +0300

    pattern facade

---
+++ patterns/facade.js
@@ -0,0 +1,60 @@
+class Conveyor {
+  setBody() {
+    console.log("Body set!");
+  }
+  getEngine() {
+    console.log("Dismantle engine!");
+  }
+  setEngine() {
+    console.log("Engine set!");
+  }
+  setInterior() {
+    console.log("Interior added!");
+  }
+  getInterior() {
+    console.log("Update interior!");
+  }
+  setExterior() {
+    console.log("Added exterior!");
+  }
+  setWheels() {
+    console.log("Wheels!");
+  }
+  addElectronics() {
+    console.log("Added electronics!");
+  }
+  paint() {
+    console.log("Car painted!");
+  }
+}
+
+class ConveyorFacade {
+  constructor(car) {
+    this.car = car;
+  }
+
+  assembleCar() {
+    this.car.setBody();
+    this.car.setEngine();
+    this.car.setInterior();
+    this.car.setExterior();
+    this.car.setWheels();
+    this.car.addElectronics();
+    this.car.paint();
+  }
+
+  changeEngine() {
+    this.car.getEngine();
+    this.car.setEngine();
+  }
+
+  changeInterior() {
+    this.car.getInterior();
+    this.car.setInterior();
+  }
+}
+
+const conveyor = new ConveyorFacade(new Conveyor());
+conveyor.assembleCar();
+conveyor.changeEngine();
+conveyor.changeInterior();
